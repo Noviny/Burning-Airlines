2e89f76d3aa659de1d71da8ed5f5f074d9414db7
Noviny BC <benjaminconolly@gmail.com>
2016-01-13 17:08:24 +1100

    search selectors firing nicely

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -3,10 +3,33 @@ var app = app || {};
 app.AppView = Backbone.View.extend({
   el: '#main',
 
+  events: {
+    'keyup': 'updateSearch'
+  },
+
+  // events: {
+  //   'click .flight_number': 'showFlight'
+  // },
+
+  // showFlight: function () {
+  //   console.log()
+  //   app.router.navigate('flights/' + this.model.get('id'), true);
+  // },
+
+
+  updateSearch: function () {
+    search = $('#searchfield').val();
+    var selectedVal = "";
+    var selected = $("input[type='radio'][name='type']:checked")
+    if (selected.length > 0 ) {
+      selectedVal = selected.val();
+    }
+    console.log(selectedVal + ' ' + search);
+
+  },
+
+
   render: function () {
-
-
-
     var searchPageTemplate = $('#searchPageTemplate').html();
     this.$el.html( searchPageTemplate );
 
