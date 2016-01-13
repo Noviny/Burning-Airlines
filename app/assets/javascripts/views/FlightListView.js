c2042358e1eeaffde93c972c64a8c297899ff6e9
Noviny BC <benjaminconolly@gmail.com>
2016-01-13 15:07:57 +1100

    flights table functional

--- app/assets/javascripts/views/FlightListView.js
+++ app/assets/javascripts/views/FlightListView.js
@@ -2,12 +2,28 @@ var app = app || {};
 
 
 app.FlightListView = Backbone.View.extend({
-  tagName: "li",
+  el: "#flightlist",
+
+  events: {
+    'click .flight_number': 'showFlight'
+  },
+
+  showFlight: function () {
+    console.log(this.model)
+    app.router.navigate('flights/' + this.model.get('id'), true);
+  },
 
   render: function () {
     console.log("rendered")
-    var name = this.model.get("flight_no");
-    this.$el.text( name );
-    this.$el.prependTo("#flights")
+    // var flight_no = this.model.get("flight_no");
+    // var origin = this.model.get("origin");
+    // var destination = this.model.get("destination");
+    // var date = this.model.get("date");
+    // this.$el.text( name );
+    // this.$el.prependTo("#flights")
+
+    var FlightViewTemplater = _.template( $("#listFlightTemplate").html() );
+    this.$el.prepend(FlightViewTemplater( this.model.toJSON() ) );
+
   }
-})
+});
