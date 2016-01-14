b6c906f972c3a2436e27db2495af5601fe9d555f
Noviny BC <benjaminconolly@gmail.com>
2016-01-14 12:09:56 +1100

    reactive search now works

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -4,7 +4,9 @@ app.AppView = Backbone.View.extend({
   el: '#main',
 
   events: {
-    'keyup': 'updateSearch'
+    'keyup': 'updateSearch',
+    'click': 'updateSearch'
+
   },
 
   // events: {
@@ -18,12 +20,23 @@ app.AppView = Backbone.View.extend({
 
 
   updateSearch: function () {
-    search = $('#searchfield').val();
+    search = $('#searchfield').val().toUpperCase();
+    var searchExp = new RegExp("^" + search)
     var selectedVal = "";
-    var selected = $("input[type='radio'][name='type']:checked")
+    var selected = $("input[type='radio'][name='type']:checked");
     if (selected.length > 0 ) {
       selectedVal = selected.val();
-    }
+    };
+    var $flights = $(".flight-data")
+    $.each($flights, function (index, el) {
+      var val = $(this).find("."+selectedVal).text();
+      console.log(val)
+      if ( searchExp.test(val) ) {
+        $(this).removeClass("hidden")
+      } else {
+        $(this).addClass("hidden")
+      }
+    });
     console.log(selectedVal + ' ' + search);
 
   },
